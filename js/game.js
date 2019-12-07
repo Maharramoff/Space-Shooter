168085489547b20ebe14ba4d6031b588a5fb80f0
Maharramoff <sh.maharramov@gmail.com>
2019-12-07 22:08:37 +0400

    update: sprite handler and asteroid bounds

--- js/game.js
+++ js/game.js
@@ -4,6 +4,11 @@ const context = canvas.getContext('2d');
 const backgroundImage = new Image();
 backgroundImage.src = 'img/space.png';
 
+// Load the asteroid tileset
+let gameTileset = new Image();
+gameTileset.src = 'img/sheet.png';
+
+
 window.onload = function ()
 {
     game();
@@ -16,14 +21,34 @@ function game()
     requestAnimationFrame(game);
 }
 
+let asteroid = {x: 0, y: 250, dx: 1, dy: 2, h: 45, w: 55};
+
 function update()
 {
+    asteroid.x += asteroid.dx;
+    asteroid.y += asteroid.dy;
 
+    // Asteroid bounds
+    if (asteroid.x + asteroid.w >= 600 || asteroid.x <= 0) asteroid.dx = -asteroid.dx;
+    if (asteroid.y + asteroid.h >= 600 || asteroid.y <= 0) asteroid.dy = -asteroid.dy;
 }
 
 function draw()
 {
+    // Draw background
     context.drawImage(backgroundImage, 0, 0, 600, 600);
+
+    // Draw asteroids
+    context.drawImage(
+      gameTileset, //The image file
+      0, 618, //The source x and y position
+      119, 97, //The source height and width
+      asteroid.x, asteroid.y, //The destination x and y position
+      asteroid.h, asteroid.w //The destination height and width
+    );
+
+
+
 }
 
 // requestAnimationFrame polyfill
@@ -35,7 +60,7 @@ if (!window.requestAnimationFrame)
           window.mozRequestAnimationFrame ||
           window.oRequestAnimationFrame ||
           window.msRequestAnimationFrame ||
-          // Əgər heç bir brauzer dəstəkləməsə, köhnə metod istifadə edirik.
+          // Old style method
           function (callback, element)
           {
               window.setTimeout(callback, 1000 / 60);
