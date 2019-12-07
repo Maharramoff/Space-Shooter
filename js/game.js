097f2d8d2b2ccd893a9c74a65e409320ae35df58
Maharramoff <sh.maharramov@gmail.com>
2019-12-08 01:18:41 +0400

    update: asteroids spawning randomly

--- js/game.js
+++ js/game.js
@@ -1,5 +1,7 @@
 const canvas = document.getElementById('game');
 const context = canvas.getContext('2d');
+const spawnFrame = 30;
+let timer = 0;
 
 const backgroundImage = new Image();
 backgroundImage.src = 'img/space.png';
@@ -20,23 +22,41 @@ function game()
     requestAnimationFrame(game);
 }
 
-let asteroids = [
-    { x: 0, y: 250, sx: 0, sy: 618, sw: 119, sh: 97, dx: 10, dy: 20, h: 45, w: 55 },
-    { x: 0, y: 250, sx: 326, sy: 549, sw: 99, sh: 95, dx: 5, dy: 10, h: 53, w: 55 },
+const asteroids = [
+    { sx: 0, sy: 618, sw: 119, sh: 97, h: 45, w: 55 },
+    { sx: 326, sy: 549, sw: 99, sh: 95, h: 53, w: 55 },
+    { sx: 224, sy: 748, sw: 100, sh: 84, h: 46, w: 55 },
 ];
+
+let asteroidList = [];
 
 function update()
 {
-    for (let i in asteroids)
-    {
-        asteroids[i].x += asteroids[i].dx;
-        asteroids[i].y += asteroids[i].dy;
+    timer++;
 
-        // Asteroid bounds
-        if (asteroids[i].x + asteroids[i].w >= 600 || asteroids[i].x <= 0) asteroids[i].dx = -asteroids[i].dx;
-        if (asteroids[i].y + asteroids[i].h >= 600 || asteroids[i].y <= 0) asteroids[i].dy = -asteroids[i].dy;
+    if (timer % spawnFrame === 0)
+    {
+        let genObject = {
+            x: Math.random() * 600,
+            y: -50,
+            dx: Math.random() * 2 - 1,
+            dy: Math.random() * 2 + 2,
+        };
+
+        let randomAsteroid = asteroids[Math.floor(Math.random() * asteroids.length)];
+
+        asteroidList.push({...randomAsteroid, ...genObject});
     }
 
+    for (let i in asteroidList)
+    {
+        // Asteroids physics
+        asteroidList[i].x += asteroidList[i].dx;
+        asteroidList[i].y += asteroidList[i].dy;
+
+        // Remove the asteroids that are out of bounds.
+        if (asteroidList[i].x + asteroidList[i].w < 0 || asteroidList[i].x > 600 || asteroidList[i].y > 600) asteroidList.splice(i, 1);
+    }
 }
 
 function draw()
@@ -44,15 +64,15 @@ function draw()
     // Draw background
     context.drawImage(backgroundImage, 0, 0, 600, 600);
 
-    for (let i in asteroids)
+    for (let i in asteroidList)
     {
         // Draw asteroids
         context.drawImage(
           gameTileset, //The image file
-          asteroids[i].sx, asteroids[i].sy, //The source x and y position
-          asteroids[i].sw, asteroids[i].sh, //The source width and height
-          asteroids[i].x, asteroids[i].y, //The destination x and y position
-          asteroids[i].h, asteroids[i].w //The destination height and width
+          asteroidList[i].sx, asteroidList[i].sy, //The source x and y position
+          asteroidList[i].sw, asteroidList[i].sh, //The source width and height
+          asteroidList[i].x, asteroidList[i].y, //The destination x and y position
+          asteroidList[i].h, asteroidList[i].w //The destination height and width
         );
     }
 }
