91f8dff091a85a9ec1bfb84cfa87821702833552
Maharramoff <sh.maharramov@gmail.com>
2019-12-10 20:36:52 +0400

    update: asteroids explosion and particles handling

--- js/game.js
+++ js/game.js
@@ -21,8 +21,15 @@ document.addEventListener('mousedown', mouseClick, false)
 const ship = { sx: 0, sy: 942, sw: 112, sh: 74, h: 40, w: 60 };
 const bullet = { sx: 856, sy: 602, sw: 9, sh: 37, h: 37, w: 9 };
 const bulletExplosion = [
-  { sx: 603, sy: 600, sw: 46, sh: 46, h: 46, w: 46 },
-  { sx: 581, sy: 661, sw: 46, sh: 46, h: 46, w: 46 },
+    { sx: 603, sy: 600, sw: 46, sh: 46, h: 46, w: 46 },
+    { sx: 581, sy: 661, sw: 46, sh: 46, h: 46, w: 46 },
+];
+const particle = [
+    { sx: 283, sy: 453, sw: 44, sh: 39, h: 44, w: 39 },
+    { sx: 396, sy: 414, sw: 29, sh: 25, h: 29, w: 25 },
+    { sx: 602, sy: 646, sw: 15, sh: 15, h: 15, w: 15 },
+    { sx: 365, sy: 814, sw: 16, sh: 18, h: 16, w: 18 },
+    { sx: 407, sy: 263, sw: 26, sh: 25, h: 26, w: 25 },
 ];
 const asteroids = [
     { sx: 0, sy: 618, sw: 119, sh: 97, h: 45, w: 55 },
@@ -45,9 +52,11 @@ function game()
 }
 
 let asteroidList = [];
-let bulletList   = [];
+let bulletList = [];
 let bulletExplosionList = [];
+let particleList = [];
 let timer = 0;
+let particleLength = particle.length;
 
 function update()
 {
@@ -72,7 +81,6 @@ function update()
         asteroidList.push({ ...randomAsteroid, ...genObject });
     }
 
-
     for (let i in asteroidList)
     {
         // Asteroids physics
@@ -82,11 +90,10 @@ function update()
         // Remove the asteroids that are out of stage.
         if (asteroidList[i].x + asteroidList[i].w < 0 || asteroidList[i].x > 600 || asteroidList[i].y > 600) asteroidList.splice(i, 1);
 
-
         // Check if target shot down
         for (let b in bulletList)
         {
-            if(asteroidList.hasOwnProperty(i) && bulletList.hasOwnProperty(b))
+            if (asteroidList.hasOwnProperty(i) && bulletList.hasOwnProperty(b))
             {
                 if (
                   bulletList[b].x > asteroidList[i].x
@@ -97,7 +104,24 @@ function update()
                 )
                 {
                     // Bullet explosion
-                    bulletExplosionList.push({x: bulletList[b].x, y: bulletList[b].y});
+                    bulletExplosionList.push({ x: bulletList[b].x, y: bulletList[b].y });
+
+                    // Particles
+                    let particleObjects = [];
+
+                    for (let p = 0; p < particleLength; p++)
+                    {
+                        particleObjects.push(
+                          {
+                              x : bulletList[b].x,
+                              y : bulletList[b].y,
+                              dx: (particle[p].w / 20) * Math.cos((p * 360 / particleLength) * (Math.PI / 180)),
+                              dy: (particle[p].w / 20) * Math.sin((p * 360 / particleLength) * (Math.PI / 180)),
+                          }
+                        );
+                    }
+
+                    particleList.push(particleObjects);
 
                     // Remove collided elements
                     bulletList.splice(b, 1);
@@ -115,6 +139,31 @@ function update()
 
         // Remove the bullets that are out of stage.
         if (bulletList[b].x + bulletList[b].w < 0 || bulletList[b].x > 600 || bulletList[b].y < 0) bulletList.splice(b, 1);
+    }
+
+    for (let p in particleList)
+    {
+
+        // Əgər hissənin listi boş deyilsə
+        if (particleList[p].filter(function(value) { return value !== undefined }).length)
+        {
+            for (let j in particleList[p])
+            {
+                if (particleList[p][j] !== undefined)
+                {
+                    // Particle physics
+                    particleList[p][j].x += particleList[p][j].dx;
+                    particleList[p][j].y += particleList[p][j].dy;
+
+                    // Remove the particles that are out of stage.
+                    if (particleList[p][j].x + particle[j].w < 0 || particleList[p][j].x > 600 || particleList[p][j].y > 600 || particleList[p][j].y + particle[j].h < 0) particleList[p][j] = undefined;
+                }
+            }
+        }
+        else
+        {
+            particleList.splice(p, 1);
+        }
     }
 }
 
@@ -149,6 +198,24 @@ function draw()
           bulletExplosionList[e].x - bulletExplosion[1].w / 2, bulletExplosionList[e].y - bulletExplosion[1].h / 2,
           bulletExplosion[1].w, bulletExplosion[1].h
         );
+    }
+
+    // Draw particles
+    for (let p in particleList)
+    {
+        for (let j in particleList[p])
+        {
+            if (particleList[p][j] !== undefined)
+            {
+                context.drawImage(
+                  spriteSheet,
+                  particle[j].sx, particle[j].sy,
+                  particle[j].sw, particle[j].sh,
+                  particleList[p][j].x, particleList[p][j].y,
+                  particle[j].w, particle[j].h
+                );
+            }
+        }
     }
 
     for (let i in asteroidList)
@@ -191,6 +258,13 @@ function mouseClick(event)
           dx: 0,
           dy: bulletSpeed,
       });
+}
+
+function getRandomInt(min, max)
+{
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // requestAnimationFrame polyfill
